d02847defce3440d87b05e0ff9245ec88a57fb9f
kathsup <katherinencarvallo22@gmail.com>
2025-03-21 14:21:09 -0600

    musica y botones funcionando

--- core/src/main/java/com/musica/mp3/MP3.java
+++ core/src/main/java/com/musica/mp3/MP3.java
@@ -1,13 +1,103 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.musica.mp3;
 
-/**
- *
- * @author Lenovo
- */
-public class MP3 {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class MP3 implements Screen{
+
+    private Texture play, pause, stop;
+    private float btnWidth, btnHeight;
+    private SpriteBatch batch;
+    private Vector2 posBtnplay, posBtnpause, posBtnstop;
+    private main reproductor;
+    
+    public MP3(main reproductor) {  // Recibe el objeto 'main' al crearse
+        this.reproductor = reproductor;  // Guardar la referencia
+    }
+    
+    @Override
+    public void show() {
+    batch = new SpriteBatch(); 
+    play = new Texture("play.jpg");
+    pause = new Texture("pause.jpg");
+    stop = new Texture("stop.jpg");
+    
+        posBtnplay = new Vector2(400, 160);
+        posBtnpause = new Vector2(500, 160);
+        posBtnstop = new Vector2(300, 160);
+    
+        btnWidth = 50;  
+        btnHeight = 50; 
+    }
+
+    @Override
+    public void render(float f) {
+    ScreenUtils.clear(1, 1, 1, 1);    
+    batch.begin();
+    
+    batch.draw(play, posBtnplay.x, posBtnplay.y, btnWidth, btnHeight);
+    batch.draw(pause, posBtnpause.x, posBtnpause.y, btnWidth, btnHeight);
+    batch.draw(stop, posBtnstop.x, posBtnstop.y, btnWidth, btnHeight);
+    batch.end();
+    
+    if (Gdx.input.isTouched()) {
+        Vector2 touchPos = new Vector2(Gdx.input.getX(), Gdx.input.getY());
+        touchPos.y = Gdx.graphics.getHeight() - touchPos.y;
+
+        
+            if (isButtonPressed(touchPos, posBtnplay, btnWidth, btnHeight)) {
+                System.out.println("Botón Play presionado");
+                reproductor.musica.play();
+                
+               
+            } else if (isButtonPressed(touchPos, posBtnpause, btnWidth, btnHeight)) {
+                System.out.println("Botón Pause presionado");
+                reproductor.musica.pause();
+                
+            } else if (isButtonPressed(touchPos, posBtnstop, btnWidth, btnHeight)) {
+                System.out.println("Botón Stop presionado");
+                reproductor.musica.play();
+                reproductor.musica.stop(); 
+                
+            }
+    }
+    }
+
+   
+    
+    private boolean isButtonPressed(Vector2 touchPos, Vector2 buttonPos, float buttonWidth, float buttonHeight) {
+        return touchPos.x > buttonPos.x && touchPos.x < buttonPos.x + buttonWidth
+                && touchPos.y > buttonPos.y && touchPos.y < buttonPos.y + buttonHeight;
+    }
+    
+    @Override
+    public void resize(int i, int i1) {
+    }
+
+    @Override
+    public void pause() {
+    }
+
+    @Override
+    public void resume() {
+    }
+
+    @Override
+    public void hide() {
+    }
+
+    @Override
+    public void dispose() {
+        batch.dispose();
+        pause.dispose();
+        play.dispose();
+        stop.dispose();
+        
+    }
     
 }
--- core/src/main/java/com/musica/mp3/main.java
+++ core/src/main/java/com/musica/mp3/main.java
@@ -1,34 +1,64 @@
 package com.musica.mp3;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+public class main extends Game {
+    //private SpriteBatch batch;
+    //private Texture image;
+    public Music musica; 
+    public boolean sonidoActivado = true;
+    
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        //batch = new SpriteBatch();
+        //image = new Texture("libgdx.png");
+        setScreen(new MP3(this));
+        musica = Gdx.audio.newMusic(Gdx.files.internal("musica.mp3"));
+         musica.setLooping(true);
+        musica.setVolume(0.5f);
+         if (sonidoActivado) {
+            musica.play();
+        }
     }
 
+    
+    public void alternarSonido() {
+        sonidoActivado = !sonidoActivado;
+
+        
+        if (sonidoActivado) {
+            musica.play();
+        } else {
+            musica.pause(); 
+        }
+    }
+    
+    
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
+        /*batch.begin();
         batch.draw(image, 140, 210);
-        batch.end();
+        batch.end();*/
+        super.render();
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        image.dispose();
+       /* batch.dispose();
+        image.dispose();*/
+       super.dispose();
+       if (musica != null) {
+            musica.dispose();
+        }
     }
 }
